7a545d9febf77bda1b94d99da6e6cc126a4c9993
{dp7jm} <{dpjm@chef.net}>
2019-07-25 22:13:18 +0200

    created randome number generator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import SlotMachine from './SlotMachine';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
-      
+      <SlotMachine
+        s1="x"
+        s2="2"
+        s3="3"
+      />
     </div>
   );
 }
--- src/SlotMachine.js
+++ src/SlotMachine.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 class SlotMachine extends React.Component {
     render() {
+        const randomNumber = Math.floor(Math.random() * 3);
+        const slotSigns = ["x" , "y" , "z"];
         return (
-            <div>
-                heloo friend
+            <div>              
+              
+                {slotSigns[randomNumber]}
+                
             </div>
 
         );

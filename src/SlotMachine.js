0e5e42b43c90ba9e863b4a0c17c05156a433c809
{dp7jm} <{dpjm@chef.net}>
2019-07-25 21:08:16 +0200

    SlotMachine upgrades

---
+++ src/SlotMachine.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SlotMachine extends React.Component {
+    rednder() {
+        return(
+            <div>Hello my firend</div>
+        );
+    }
+}
+
+export default SlotMachine;
